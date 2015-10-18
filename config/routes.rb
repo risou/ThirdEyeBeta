798153bc98601870f3aa18d12f8ad2ac8978aa2a
risou <risou.f@gmail.com>
2015-10-18 23:50:56 +0900

    use default layout in home page

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root 'home#index'
+  get 'home/index'
   get '/auth/:provider/callback', :to => 'sessions#callback'
   post '/auth/:provider/callback', :to => 'sessions#callback'
   get '/logout' => 'sessions#destroy', :as => :logout
