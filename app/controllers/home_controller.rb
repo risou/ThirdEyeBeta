92eaf17a98ab6b3633bff4aaa21a3977c0cedce0
risou <risou.f@gmail.com>
2015-10-24 23:52:57 +0900

    set test output

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < BaseController
   def index
+    client = TweetStream::Client.new
+    client.userstream do |status|
+      puts "#{status.user.name}: #{status.text}"
+    end
   end
 end
