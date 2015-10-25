c4bc3c2cf89169e4dd423bb083b85afc8c4c78ae
risou <risou.f@gmail.com>
2015-10-25 15:40:15 +0900

    get tweet sample

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < BaseController
   def index
-    client = TweetStream::Client.new
-    client.userstream do |status|
-      puts "#{status.user.name}: #{status.text}"
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = Settings.twitter.consumer_key
+      config.consumer_secret = Settings.twitter.consumer_secret
+      config.access_token = Settings.twitter.access_token
+      config.access_token_secret = Settings.twitter.access_token_secret
     end
+    @tweets = client.home_timeline
+    
+    # client = TweetStream::Client.new
+    # client.userstream do |status|
+    #   puts "#{status.user.name}: #{status.text}"
+    # end
   end
 end
