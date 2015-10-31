829944548c50a43357405a6766bba74a15e1598c
risou <risou.f@gmail.com>
2015-10-31 23:33:12 +0900

    remove unnecessary files

--- app/controllers/tweet_controller.rb
+++
@@ -1,5 +0,0 @@
-class TweetController < ApplicationController
-  def fav
-    Twitter::REST::Favorites.fav(:id)
-  end
-end
--- test/controllers/tweet_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TweetControllerTest < ActionController::TestCase
-  test "should get fav" do
-    get :fav
-    assert_response :success
-  end
-
-end
