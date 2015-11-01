5cbe1b38ba2ee0f88bca3d0879c99fd206002663
risou <risou.f@gmail.com>
2015-11-01 23:49:38 +0900

    add twitter module

---
+++ app/controllers/concerns/twitter.rb
@@ -0,0 +1,14 @@
+module Twitter
+  extend ActiveSupport::Concern
+  
+  class_method do
+    def client
+      client || = Twitter::REST::Client.new do |config|
+        config.consumer_key = Settings.twitter.consumer_key
+        config.consumer_secret = Settings.twitter.consumer_secret
+        config.access_token = Settings.twitter.access_token
+        config.access_token_secret = Settings.twitter.access_token_secret
+      end
+    end
+  end
+end
