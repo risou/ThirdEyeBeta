94f652fc107465209c698b1cbcf8325f56b78dc5
risou <risou.f@gmail.com>
2015-11-13 18:20:58 +0900

    delete unnecessary config file

--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-TwitterController.client = Twitter::REST::Client.new do |config|
-  config.consumer_key = Settings.twitter.consumer_key
-  config.consumer_secret = Settings.twitter.consumer_secret
-  config.access_token = Settings.twitter.access_token
-  config.access_token_secret = Settings.twitter.access_token_secret
-end
