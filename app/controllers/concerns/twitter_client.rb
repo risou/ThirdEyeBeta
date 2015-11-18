b299b0f100db453c3bb89579b3e46dfcab5b9855
risou <risou.f@gmail.com>
2015-11-18 20:27:00 +0900

    add stream variable

--- app/controllers/concerns/twitter_client.rb
+++ app/controllers/concerns/twitter_client.rb
@@ -10,5 +10,14 @@ module TwitterClient
         config.access_token_secret = Settings.twitter.access_token_secret
       end
     end
+
+    def stream
+      @stream ||= Twitter::Streaming::Client.new do |config|
+        config.consumer_key = Settings.twitter.consumer_key
+        config.consumer_secret = Settings.twitter.consumer_secret
+        config.access_token = Settings.twitter.access_token
+        config.access_token_secret = Settings.twitter.access_token_secret
+      end
+    end
   end
 end
