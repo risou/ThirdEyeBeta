d35164d43a4d933727203d8cd1cabea46e4a96fa
risou <risou.f@gmail.com>
2015-11-25 23:36:18 +0900

    set validation to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ActiveRecord::Base
+  validates :tweet_id, presence: true
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
 end
